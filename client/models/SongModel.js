7dd526fe0b55873c888f40abb3565376cf94af6b
FrankBowers24 <FrankBowers24@gmail.com>
2015-04-08 17:02:06 -0700

    working basic queue

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,17 @@ var SongModel = Backbone.Model.extend({
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function() {
+    this.trigger('enqueue', this);
+    // trigger an 'enqueue' event, AppModel should be listening for it
+  },
+  dequeue: function() {
+    // trigger a 'dequeue' event, AppModel should be listening for it
+  },
+  ended: function() {
+    this.trigger('ended', this);
   }
 
 });
